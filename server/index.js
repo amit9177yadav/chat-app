528694c1cdd1f01837de5e75a5aabf0e0110f932
Amit_Yadav <amityadav73471@gmail.com>
2023-06-03 10:55:26 +0530

    port origin change in server

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-5yw9.vercel.app",
     credentials: true,
   },
 });
